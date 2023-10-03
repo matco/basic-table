a47f0f34ffca078186c8efb1a8596720cc3dd03d
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2023-10-03 18:12:38 +0200

    Use CSS nesting

--- src/styles.js
+++ src/styles.js
@@ -22,26 +22,29 @@ th {
 	overflow: hidden;
 	text-overflow: ellipsis;
 	padding-right: 1rem;
-}
-th svg {
-	position: absolute;
-	right: 1rem;
-	top: 0.5rem;
-}
-th svg polygon {
-	fill: var(--text-color);
+
+	& svg {
+		position: absolute;
+		right: 1rem;
+		top: 0.5rem;
+
+		& polygon {
+			fill: var(--text-color);
+		}
+	}
 }
 tr {
 	border: 1px solid var(--frame-color);
-}
-tr.odd {
-	background-color: #fafafa;
-}
-tr.even {
-	background-color: white;
-}
-tr:hover {
-	background-color: #e8e8e8;
+
+	&.odd {
+		background-color: #fafafa;
+	}
+	&.even {
+		background-color: white;
+	}
+	&:hover {
+		background-color: #e8e8e8;
+	}
 }
 td {
 	overflow: hidden;
@@ -57,56 +60,62 @@ footer {
 	color: var(--text-color);
 	padding: 0.4rem;
 	margin: 0;
-}
-footer :is(.button, button) {
-	color: var(--text-color);
-	text-decoration: none;
-	background-color: #f7f7f7;
-	cursor: pointer;
-}
-footer :is(.button:hover, button:hover) {
-	background-color: #eee;
-}
-footer input {
-	margin: 0 0.5rem;
-	vertical-align: middle;
-	border-radius: 2px;
-	border: 1px solid var(--frame-highlight-color);
+
+	:is(.button, button) {
+		color: var(--text-color);
+		text-decoration: none;
+		background-color: #f7f7f7;
+		cursor: pointer;
+
+		&:hover {
+			background-color: #eee;
+		}
+	}
+	& input {
+		margin: 0 0.5rem;
+		vertical-align: middle;
+		border-radius: 2px;
+		border: 1px solid var(--frame-highlight-color);
+	}
 }
 .buttons {
 	flex-grow: 2;
-}
-.buttons .button {
-	border: 1px solid var(--frame-highlight-color);
-	border-radius: 2px;
-	padding: 0 0.5rem;
+
+	.button {
+		border: 1px solid var(--frame-highlight-color);
+		border-radius: 2px;
+		padding: 0 0.5rem;
+	}
 }
 .controls {
 	margin: 0 0.5rem;
-}
-.controls button {
-	border-color: var(--frame-highlight-color);
-	border-style: solid;
-	border-width: 1px 0 1px 1px;
-}
-.controls button[disabled] {
-	opacity: 0.1;
-	cursor: auto;
-}
-.controls button:first-child {
-	border-radius: 2px 0 0 2px;
-}
-.controls button:last-child {
-	border-radius: 0 2px 2px 0;
-	border-right-width: 1px;
-}
-.controls button svg {
-	width: 1rem;
-	height: 1rem;
-	vertical-align: bottom;
-	margin: 0.2rem 0.4rem;
-}
-.controls button svg :is(polygon,rect) {
-	fill: var(--text-color);
+
+	& button {
+		border-color: var(--frame-highlight-color);
+		border-style: solid;
+		border-width: 1px 0 1px 1px;
+
+		&[disabled] {
+			opacity: 0.1;
+			cursor: auto;
+		}
+		&:first-child {
+			border-radius: 2px 0 0 2px;
+		}
+		&:last-child {
+			border-radius: 0 2px 2px 0;
+			border-right-width: 1px;
+		}
+		& svg {
+			width: 1rem;
+			height: 1rem;
+			vertical-align: bottom;
+			margin: 0.2rem 0.4rem;
+
+			:is(polygon,rect) {
+				fill: var(--text-color);
+			}
+		}
+	}
 }
 `;
